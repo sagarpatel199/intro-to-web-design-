53e83deda23e250089b2aec111221e340f933dd1
sagarpatel199 <131931579+sagarpatel199@users.noreply.github.com>
2023-05-27 22:21:27 -0400

    Add files via upload
    this shows the concept of JS in use of mouse hover and select.

---
+++ js/gallery.js
@@ -0,0 +1,40 @@
+
+
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    /* In this function you should 
+       1) change the url for the background image of the div with the id = "image" 
+       to the source file of the preview image
+       
+       
+       2) Change the text  of the div with the id = "image" 
+       to the alt text of the preview image 
+       */
+
+       var element = document.getElementById('image');
+       element.innerHTML = previewPic.alt;
+       element.style.backgroundColor= "#CCEECC";
+       element.style.backgroundImage = "url('" + previewPic.src + "')";
+
+     
+       }
+   
+       function unDo(){
+        /* In this function you should 
+       1) Update the url for the background image of the div with the id = "image" 
+       back to the orginal-image.  You can use the css code to see what that original URL was
+       
+       2) Change the text  of the div with the id = "image" 
+       back to the original text.  You can use the html code to see what that original text was
+       */
+        var element = document.getElementById('image');
+        element.style.backgroundImage = "";
+        element.style.backgroundColor= "#8e68ff";
+        element.innerHTML = "Hover over an image below to display here.";
+           
+    
+}
+
+
+    
